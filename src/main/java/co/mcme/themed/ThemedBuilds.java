2d353b61a0cb0e87596810e8a5a724f7b854037c
Henry Slawniak <meggawatts@gmail.com>
2013-10-15 06:38:00 -0500

    Main class cleanup

--- src/main/java/co/mcme/themed/ThemedBuilds.java
+++ src/main/java/co/mcme/themed/ThemedBuilds.java
@@ -1,10 +1,9 @@
 package co.mcme.themed;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ThemedBuilds extends JavaPlugin implements Listener {
 
@@ -12,8 +11,12 @@ public class ThemedBuilds extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
         setupConfig();
+        registerEvents();
+    }
+
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     public void setupConfig() {
@@ -23,18 +26,6 @@ public class ThemedBuilds extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        final Player player = sender.getServer().getPlayer(sender.getName());
-        if (args.length != 0) {
-            String method = args[0];
-            if (method.equalsIgnoreCase("create")) {
-                if (player.hasPermission("themed.admin.create")) {
-                }
-            }
-            if (method.equalsIgnoreCase("settopic")) {
-                if (player.hasPermission("themed.admin.create")) {
-                }
-            }
-        }
         return false;
     }
 }
