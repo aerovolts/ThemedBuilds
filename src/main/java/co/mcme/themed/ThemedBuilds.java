17225f94c72705b1759a11cdc0cc78ca45602fee
Henry Slawniak <meggawatts@gmail.com>
2013-10-16 19:08:39 -0500

    GPL

--- src/main/java/co/mcme/themed/ThemedBuilds.java
+++ src/main/java/co/mcme/themed/ThemedBuilds.java
@@ -1,11 +1,23 @@
+/*  This file is part of ThemedBuilds.
+ * 
+ *  ThemedBuilds is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ThemedBuilds is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ThemedBuilds.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package co.mcme.themed;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ThemedBuilds extends JavaPlugin implements Listener {
+public class ThemedBuilds extends JavaPlugin {
 
     @Override
     public void onEnable() {
@@ -14,16 +26,10 @@ public class ThemedBuilds extends JavaPlugin implements Listener {
     }
 
     private void registerEvents() {
-        getServer().getPluginManager().registerEvents(this, this);
     }
 
     public void setupConfig() {
         getConfig().options().copyDefaults(true);
         saveConfig();
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        return false;
     }
 }
