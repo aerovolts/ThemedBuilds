f02ed8972c1ead4e13913c8e81170f4693aac2f4
Henry Slawniak <meggawatts@gmail.com>
2014-04-12 15:39:58 -0500

    Revert "Nuke"
    This reverts commit c47975dd26d63b985226cf2295d03eb44bbf2b02.

---
+++ src/main/java/co/mcme/util/jackson/serialization/ObjectIdJsonDeserializer.java
@@ -0,0 +1,32 @@
+/*  This file is part of ThemedBuilds.
+ * 
+ *  ThemedBuilds is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ThemedBuilds is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ThemedBuilds.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.mcme.util.jackson.serialization;
+
+import java.io.IOException;
+import org.bson.types.ObjectId;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.DeserializationContext;
+import org.codehaus.jackson.map.JsonDeserializer;
+
+public class ObjectIdJsonDeserializer extends JsonDeserializer<ObjectId> {
+
+    @Override
+    public ObjectId deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        return new ObjectId(jp.getText());
+    }
+
+}
