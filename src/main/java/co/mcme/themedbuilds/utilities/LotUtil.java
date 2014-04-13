c675f16b7e6c01b7d2d994073ccb0c4b9b4093da
Henry Slawniak <meggawatts@gmail.com>
2014-04-13 08:00:59 -0500

    Update LotUtil

--- src/main/java/co/mcme/themedbuilds/utilities/LotUtil.java
+++ src/main/java/co/mcme/themedbuilds/utilities/LotUtil.java
@@ -17,17 +17,29 @@ package co.mcme.themedbuilds.utilities;
 
 import co.mcme.themedbuilds.ThemedBuildPlugin;
 import co.mcme.themedbuilds.database.Corner;
+import co.mcme.themedbuilds.database.Lot;
 import co.mcme.themedbuilds.database.Theme;
 import com.mongodb.BasicDBObject;
 import java.io.IOException;
 
 public class LotUtil {
 
-    public Theme getThemeByCoordinate(int zcoord) {
+    public Theme getThemeByExactZCoordinate(int zcoord) {
         Corner lookup = new Corner(0, zcoord);
         try {
             Theme theme = ThemedBuildPlugin.getJsonMapper().readValue(ThemedBuildPlugin.getMongoUtil().getThemeCollection().findOne(new BasicDBObject("corner", lookup.toString())).toString(), Theme.class);
             return theme;
+        } catch (IOException ex) {
+            ThemedLogger.severe(ex.getMessage());
+        }
+        return null;
+    }
+
+    public Lot getLotByExactCornerCoordinate(int xcoord, int zcoord) {
+        Corner lookup = new Corner(xcoord, zcoord);
+        try {
+            Lot lot = ThemedBuildPlugin.getJsonMapper().readValue(ThemedBuildPlugin.getMongoUtil().getLotCollection().findOne(new BasicDBObject("corner", lookup)).toString(), Lot.class);
+            return lot;
         } catch (IOException ex) {
             ThemedLogger.severe(ex.getMessage());
         }
