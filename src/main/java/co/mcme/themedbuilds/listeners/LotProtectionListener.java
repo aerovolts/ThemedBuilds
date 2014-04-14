c971eade4fa636cc52b71cdfdf3975df8edfea2b
Henry Slawniak <meggawatts@gmail.com>
2014-04-14 16:53:37 -0500

    Fetch and check lot perms

--- src/main/java/co/mcme/themedbuilds/listeners/LotProtectionListener.java
+++ src/main/java/co/mcme/themedbuilds/listeners/LotProtectionListener.java
@@ -19,6 +19,7 @@ import co.mcme.themedbuilds.ThemedBuildPlugin;
 import co.mcme.themedbuilds.database.Corner;
 import co.mcme.themedbuilds.database.Lot;
 import co.mcme.themedbuilds.database.Theme;
+import co.mcme.themedbuilds.utilities.DatabaseUtil;
 import co.mcme.themedbuilds.utilities.ThemedLogger;
 import com.sk89q.worldedit.Vector;
 import org.bukkit.ChatColor;
@@ -26,31 +27,58 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class LotProtectionListener implements Listener {
 
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
+        if (event.getPlayer().hasPermission("themedbuilds.ignoreprotection")) {
+            return;
+        }
         Theme currtheme = ThemedBuildPlugin.getCurrentTheme();
         Block b = event.getBlock();
         ThemedLogger.info(currtheme.getBounds().toString());
         if (currtheme.getBounds().contains(new Vector(b.getX(), b.getY(), b.getZ()))) {
-            // In current build
-            // Lot size * size 6 blocks inbetween
             int lotindex = (int) Math.floor(b.getX() / (currtheme.getLotsize() + 6));
-            //This is dumb, instead calculate lot corner and fetch from db
             int cornerz = lotindex * currtheme.getLotsize() + 6;
             if (lotindex == 0) {
                 cornerz = 0;
             }
             Corner corner = new Corner(currtheme.getCorner().getX(), cornerz);
+            Lot lot = DatabaseUtil.getLotByCorner(corner);
+            if (lot.getOwner().equals(event.getPlayer())) {
+            } else {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.RED + "You are not allowed to build in " + lot.getOwner().getName() + "'s lot");
+            }
+        } else {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "You are not in the build area for the current themedbuild");
+        }
+    }
 
-//            if (lot.getOwner().equals(event.getPlayer())) {
-//
-//            } else {
-//                event.setCancelled(true);
-//                event.getPlayer().sendMessage(ChatColor.RED + "You are not allowed to build in " + lot.getOwner().getName() + "'s lot");
-//            }
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
+        if (event.getPlayer().hasPermission("themedbuilds.ignoreprotection")) {
+            return;
+        }
+        Theme currtheme = ThemedBuildPlugin.getCurrentTheme();
+        Block b = event.getBlock();
+        ThemedLogger.info(currtheme.getBounds().toString());
+        if (currtheme.getBounds().contains(new Vector(b.getX(), b.getY(), b.getZ()))) {
+            int lotindex = (int) Math.floor(b.getX() / (currtheme.getLotsize() + 6));
+            int cornerz = lotindex * currtheme.getLotsize() + 6;
+            if (lotindex == 0) {
+                cornerz = 0;
+            }
+            Corner corner = new Corner(currtheme.getCorner().getX(), cornerz);
+            Lot lot = DatabaseUtil.getLotByCorner(corner);
+            if (lot.getOwner().equals(event.getPlayer())) {
+            } else {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.RED + "You are not allowed to build in " + lot.getOwner().getName() + "'s lot");
+            }
         } else {
             event.setCancelled(true);
             event.getPlayer().sendMessage(ChatColor.RED + "You are not in the build area for the current themedbuild");
